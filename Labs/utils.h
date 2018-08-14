9e70db82681b9764ec391e0d3d5f65ad7f10e6bd
Michael Bradley <mbrad94@gmail.com>
2018-08-14 11:59:59 +0200

    Create utils header file

---
+++ Labs/utils.h
@@ -0,0 +1,12 @@
+#ifndef MJB_UTILS
+#define MJB_UTILS
+
+unsigned char GetBit(unsigned char x, unsigned char k) {
+    return ((x & (0x01 << k)) != 0);
+}
+
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+    return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+
+#endif
