ab00149d1aa25155346f73b297c33b87c78d04de
Michael Bradley <mbrad94@gmail.com>
2018-08-18 15:13:11 +0200

    Add parens to increase clarity in utils.h

--- Labs/utils.h
+++ Labs/utils.h
@@ -6,7 +6,7 @@ unsigned char GetBit(unsigned char x, unsigned char k) {
 }
 
 unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
-    return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+    return (b ? (x | (0x01 << k)) : (x & ~(0x01 << k)));
 }
 
 #endif
