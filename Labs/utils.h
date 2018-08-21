b72b585b2ba83c58bb294bb2078978c5435f0b88
Michael Bradley <mbrad94@gmail.com>
2018-08-21 16:55:49 +0200

    Add functionality for multiple tasks

--- Labs/utils.h
+++ Labs/utils.h
@@ -9,4 +9,21 @@ unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
     return (b ? (x | (0x01 << k)) : (x & ~(0x01 << k)));
 }
 
+typedef struct task {
+	int state;
+	unsigned long period;
+	unsigned long elapsedTime;
+	int (*Tick)(int);
+} task;
+
+task CreateTask(int state, unsigned long period, int (*Tick)(int)) {
+	task ret;
+	ret.state = state;
+	ret.period = period;
+	ret.elapsedTime = ret.period;
+	ret.Tick = Tick;
+
+	return ret;
+}
+
 #endif
