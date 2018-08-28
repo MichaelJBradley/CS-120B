ac33c20f903fb45bb79ce348f662b35956ad19a8
Michael Bradley <mbrad94@gmail.com>
2018-08-28 19:54:42 +0200

    Create global variables header

---
+++ Labs/final_project_pong/final_project_pong/src/globals.h
@@ -0,0 +1,15 @@
+/*
+ * globals.h
+ *
+ * Created: 2018-08-28 5:02:22 PM
+ *  Author: Michael Bradley
+ */ 
+
+
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+unsigned char rows_g;
+unsigned char cols_g;
+
+#endif /* GLOBALS_H_ */
