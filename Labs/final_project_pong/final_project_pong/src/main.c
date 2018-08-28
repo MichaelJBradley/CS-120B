d5d7937bfe82515f2dc7b29f0d080ceb0425d9f7
Michael Bradley <mbrad94@gmail.com>
2018-08-28 19:52:48 +0200

    Remove display header test

--- Labs/final_project_pong/final_project_pong/src/main.c
+++ Labs/final_project_pong/final_project_pong/src/main.c
@@ -2,19 +2,22 @@
 #include "../../../utils.h"
 #include "display.h"
 
+#define TASKS 1
+task tasks[TASKS];
+
 const unsigned char periodGDC = 10;
 
 int main (void)
-{		
+{	
+	unsigned char left = 0;
+	
 	TimerSet(periodGDC);
 	TimerOn();
 
 	board_init();
 
 	DDRB = 0xFF;
-	PORTB = 0x00; //controls the led rows
-	DDRC = 0xFF;
-	PORTC = 0xFF; //controls the led cols; according to datasheet the matrix flips outputs
+	PORTB = 0x00;
 
 	while (1) {
 		WaitTimer();
